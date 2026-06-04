c875dd4d0ac4eca463f60180f267508bc4ebb567
lnjoy <enjoy_july@163.com>
2026-06-04 21:29:50 +0800

    update

--- sec_reviewer/core/expert_agents.py
+++ sec_reviewer/core/expert_agents.py
@@ -58,6 +58,7 @@ class BaseExpertAgent():
         messages = state.get("messages", [])
         remaining_rounds = state.get('remaining_rounds')
         rejection_history = state.get('rejection_history', {})
+        critical_history = state.get('critical_history', [])
         issue = state["issue"]
         state_update_messages = []
 
@@ -89,6 +90,11 @@ class BaseExpertAgent():
             state_update_messages = [human_msg]
         else:
             logger.info(f"[{self.expert_name}]-[issue({state['issue'].id})] 🧠 接收反馈，继续综合推理...")
+            
+            if critical_history:
+                critical_msg = f"【上次研判结论与审查意见】以下是你上一次的研判结论以及审查节点对其给出的意见。\n{critical_history[-1].model_dump_json(indent=2)}"
+                invocation_messages = [sys_msg] + messages + HumanMessage(content=critical_msg)
+            
             invocation_messages = [sys_msg] + messages
 
         # 获取绑定了工具的 LLM 实例
