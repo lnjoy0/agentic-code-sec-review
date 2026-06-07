29667a5ea952334d7cfaef444f696170a1eacb2a
lnjoy0 <enjoy_july@163.com>
2026-06-07 18:37:34 +0800

    update

--- sec_reviewer/scanners/heuristic_scanner.py
+++ sec_reviewer/scanners/heuristic_scanner.py
@@ -254,8 +254,8 @@ class HeuristicScanner:
                         res_end = cause_meta.get("EndLine")
                     
                     logger.info(result)
-                    logger.info(f"added_lines_by_file: {added_lines_by_file['res_path']}")
-                    logger.info(f"added_hunks_by_file: {added_hunks_by_file['res_path']}")
+                    logger.info(f"added_lines_by_file: {added_lines_by_file[res_path]}")
+                    logger.info(f"added_hunks_by_file: {added_hunks_by_file[res_path]}")
 
                 if not res_path:
                     continue
