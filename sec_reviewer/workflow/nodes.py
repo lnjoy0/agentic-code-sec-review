bb4759d848b76dbc1d06e3f22b313897c4ac2d40
lnjoy0 <enjoy_july@163.com>
2026-06-16 00:26:01 +0800

    update

--- sec_reviewer/workflow/nodes.py
+++ sec_reviewer/workflow/nodes.py
@@ -123,6 +123,8 @@ async def dynamic_router_node(state: AuditState, config: RunnableConfig):
             else:
                 continue # 重复的漏洞不再路由
 
+    raise('test')
+
     # 去重后的待研判漏洞总数
     total_target_issues = len(unique_issues)
     
